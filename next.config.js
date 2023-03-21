326ae8928a008647815ed75218a1f6d19ec329f9
Filip Condac <filipcondac@gmail.com>
2023-03-21 14:01:55 +0000

    deploy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,24 @@
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 module.exports = {
-  webpack(config, options) {
-    // Disable TypeScript type checking in production builds
-    if (!options.dev) {
-      config.plugins = config.plugins.filter(
-        (plugin) => !(plugin instanceof ForkTsCheckerWebpackPlugin)
-      );
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
     }
 
     return config;
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
+
+// eslint-disable-next-line no-undef
+module.exports = nextConfig;
